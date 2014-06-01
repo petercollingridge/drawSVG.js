0eaa606c7eb436e25e5b0e876787e84ac71565e3
peter <peter.collingridge@gmail.com>
2014-06-01 13:39:21 +0100

    Update tests to include drawMolecule.js test

--- testing/test.js
+++ testing/test.js
@@ -2,13 +2,13 @@ var testDrawSVG = function(id) {
     // Create SVG
     var mySVG = new SVG({ width: 400, height: 200 });
 
+    // Draw a background
+    mySVG.rect(0, 0, 400, 200, { fill: '#ddf' });
+
     // Create a group
     var myGroup = mySVG.addChild('g', { opacity: 0.5 });
-
-    // Draw some rects
-    myGroup.rect(100, 50, 200, 100, { fill: '#88f', onMouseOver: "this.setAttribute('fill', '#f22');" });
-    myGroup.circle(100, 50, 50, { fill: '#8ff' });
-    myGroup.ellipse(200, 50, 80, 40, { fill: '#f8f' });
+    myGroup.circle(80, 100, 70, { fill: '#8ff', onMouseOver: "this.setAttribute('fill', '#f22');" });
+    myGroup.ellipse(280, 100, 100, 70, { fill: '#f8f' });
     myGroup.line(200, 50, 80, 40, { stroke: '#000' });
 
     mySVG.polyline([100, 100, 100, 160, 140, 130], { stroke: '#00f', fill: 'none' });
@@ -17,8 +17,23 @@ var testDrawSVG = function(id) {
 
     // Render
     mySVG.show('#mySVG');
-}
+};
+
+var testMolecule = function() {
+    var water = new MoleculeSVG();
+
+    water.addAtoms([
+        ['O', -0.064, 0,  0],
+        ['H',  0.512, 0, -0.77],
+        ['H',  0.512, 0,  0.77]
+    ]);
+
+    // Create a bond between atoms 0 and 1, and between atoms 0 and 2
+    water.addBonds([[0, 1], [0, 2]]);
+
+    water.draw('moleculeSVG');
+};
 
 $(document).ready(function() {
-    testDrawSVG();
+    testMolecule();
 });
